1cd6a729734136368504b92ce073f539fbba5493
Emrah ESEN <emrhesen@gmail.com>
2019-07-06 22:45:44 +0300

    IoC Container & Initial Banking Project

---
+++ MicroServices.Infra.IoC/DependencyContainer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Extensions.DependencyInjection;
+using MicroServices.Domain.Core.Bus;
+using MicroServices.Infra.Bus;
+
+namespace MicroServices.Infra.IoC
+{
+    public class DependencyContainer
+    {
+        public static void RegisterServices(IServiceCollection services)
+        {
+            // Domain Bus
+            services.AddTransient<IEventBus, RabbitMQBus>();
+
+
+        }
+    }
+}
